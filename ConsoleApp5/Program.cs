af409ad24283f5b8fe7750bf745100a33027260f
Timur Gavryukov <timypka@yandex.ru>
2021-11-27 18:54:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Person person = new Person();
+person.Print();
+
+class Person
+{
+
+    public void Print()
+    {
+        Console.WriteLine("Привет");
+    }
+
+}
+
